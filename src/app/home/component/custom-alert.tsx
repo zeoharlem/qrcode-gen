a8d1340df05711fa50d4e1bd65d75d711858f611
theophilus-alamu <theophilus.alamu8@gmail.com>
2025-08-31 00:46:30 +0100

    added alert dialog

---
+++ src/app/home/component/custom-alert.tsx
@@ -0,0 +1,32 @@
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+
+export default function CustomAlertDialog() {
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger>Open</AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        This action cannot be undone. This will permanently delete your account
+                        and remove your data from our servers.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>OK</AlertDialogCancel>
+                    {/*<AlertDialogAction>Continue</AlertDialogAction>*/}
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}
